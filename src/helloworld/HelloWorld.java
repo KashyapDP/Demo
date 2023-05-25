4be96a0a3496207c83d811a34e1ecec2ad9bb128
KashyapDP <134000677+KashyapDP@users.noreply.github.com>
2023-05-25 13:29:01 -0400

    Update HelloWorld.java
    change print statement

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -15,7 +15,7 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello");
+        System.out.println("Hello Bhai Kem cho?");
         // this program is ready
     }
 }
